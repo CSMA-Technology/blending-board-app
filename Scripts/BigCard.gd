65f0b35fd8b40e638e74ebd08518c21ed399fabf
Stephanie Cruz <stephanie@csma.technology>
2022-12-22 10:39:02 -0500

    feat(minimize-tray): minimize card tray, click big cards to switch (#16)
    * minimize card drawer and move cards down
    
    * fixed wonky positioning
    
    * separate animation players and removed reset, using play backwards instead
    
    * feat(minimize-tray): minimize card tray, click big cards to switch
    
    * added a space
    
    * simplified find next card
    
    * select first card from column if big card is blank

--- Scripts/BigCard.gd
+++ Scripts/BigCard.gd
@@ -1,8 +1,21 @@
 extends Card
 
+signal big_card_clicked(data)
+
 const VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
 
 var lastVal: String
+var column: int setget set_column, get_column
+
+func set_column(columnNumber: int):
+	column = columnNumber
+	
+func get_column(): 
+	return column
+
+func update_card(cardData: CardData):
+	self.data = cardData
+	$Value.text = cardData.value
 
 func _on_Value_draw():
 	$Background.color = Color.whitesmoke
@@ -10,3 +23,7 @@ func _on_Value_draw():
 		if vowel in $Value.text.to_lower():
 			$Background.color = Color.palegoldenrod
 			break
+			
+func _on_BigCard_gui_input(event):
+	if (event is InputEventMouseButton and event.pressed) or (event is InputEventScreenTouch and event.pressed):
+		emit_signal("big_card_clicked", data)
--- Scripts/PlayDeck.gd
+++ Scripts/PlayDeck.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var deck:DeckData
+var deck: DeckData
 var bigTiles: Array
 var activeDeckId: int
 var columnCount: int
@@ -17,22 +17,47 @@ func _ready():
 	$UI/DeckName.text = deck.name
 	
 	for card in deck.cards: 
-		while card.column + 1 > $UI/CardDrawer.get_child_count():
+		while card.column + 1 > $UI/DrawerPanel/CardDrawer.get_child_count():
 			columnCount += 1
 			var new_column = Column.instance()
 			new_column.read_only = true
-			$UI/CardDrawer.add_child(new_column)
+			$UI/DrawerPanel/CardDrawer.add_child(new_column)
 		var newCard = Card.instance()
 		newCard.data = card
-		$UI/CardDrawer.get_child(card.column).add_card(newCard)
+		$UI/DrawerPanel/CardDrawer.get_child(card.column).add_card(newCard)
 		newCard.connect("card_clicked", self, "_on_Card_clicked")
 	
-	for n in columnCount: 
-		$UI/BigCards.add_child(BigCard.instance())
+	for n in columnCount:
+		var newBigCard = BigCard.instance()
+		newBigCard.update_card(CardData.new("", n))
+		$UI/BigCardsArea/BigCards.add_child(newBigCard)
+		newBigCard.connect("big_card_clicked", self, "_on_BigCard_clicked")
 
 func _on_Card_clicked(cardData):
-	var column = cardData.column
-	$UI/BigCards.get_child(column).get_node("Value").text = cardData.value
+	set_big_card_value(cardData)
+
+func _on_BigCard_clicked(data):
+	var newData = find_next_card_in_column(data)
+	set_big_card_value(newData) 
+
+func find_next_card_in_column(data: CardData):
+	var startingIdx = deck.cards.find(data)
+	for cardIdx in range(startingIdx + 1, deck.cards.size() + startingIdx):
+		var card = deck.cards[cardIdx % deck.cards.size()]
+		if card.column == data.column:
+			return card
+	return deck.cards[0]
 
 func _on_Home_pressed():
 	get_tree().change_scene("res://Scenes/Home.tscn")
+
+func _on_Minimize_toggled(button_pressed):
+	if(button_pressed):
+		$UI/DrawerPanel/AnimationPlayer.play("minimize_drawer")
+		$UI/BigCardsArea/AnimationPlayer.play("slide_cards_down")
+	else:
+		$UI/DrawerPanel/AnimationPlayer.play_backwards("minimize_drawer")
+		$UI/BigCardsArea/AnimationPlayer.play_backwards("slide_cards_down")
+
+func set_big_card_value(cardData: CardData):
+	$UI/BigCardsArea/BigCards.get_child(cardData.column).update_card(cardData)
