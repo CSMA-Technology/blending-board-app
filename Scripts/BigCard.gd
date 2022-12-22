7f3a859def6acecee79d6bfb7af9dab3e1df0eb2
Stephanie Cruz <exscruzme@gmail.com>
2022-12-21 21:54:31 -0500

    feat(minimize-tray): minimize card tray, click big cards to switch

--- Scripts/BigCard.gd
+++ Scripts/BigCard.gd
@@ -1,8 +1,17 @@
 extends Card
 
+signal big_card_clicked(column, value)
+
 const VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
 
 var lastVal: String
+var column: int setget set_column, get_column
+
+func set_column(columnNumber: int):
+	column = columnNumber
+	
+func get_column(): 
+	return column
 
 func _on_Value_draw():
 	$Background.color = Color.whitesmoke
@@ -10,3 +19,8 @@ func _on_Value_draw():
 		if vowel in $Value.text.to_lower():
 			$Background.color = Color.palegoldenrod
 			break
+
+
+func _on_BigCard_gui_input(event):
+	if (event is InputEventMouseButton and event.pressed) or (event is InputEventScreenTouch and event.pressed):
+		emit_signal("big_card_clicked", self.column, $Value.text)
--- Scripts/PlayDeck.gd
+++ Scripts/PlayDeck.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var deck:DeckData
+var deck: DeckData
 var bigTiles: Array
 var activeDeckId: int
 var columnCount: int
@@ -27,20 +27,47 @@ func _ready():
 		$UI/DrawerPanel/CardDrawer.get_child(card.column).add_card(newCard)
 		newCard.connect("card_clicked", self, "_on_Card_clicked")
 	
-	for n in columnCount: 
-		$UI/BigCards.add_child(BigCard.instance())
+	for n in columnCount:
+		var newBigCard = BigCard.instance()
+		newBigCard.set_column(n)
+		$UI/BigCardsArea/BigCards.add_child(newBigCard)
+		newBigCard.connect("big_card_clicked", self, "_on_BigCard_clicked")
 
 func _on_Card_clicked(cardData):
 	var column = cardData.column
-	$UI/BigCards.get_child(column).get_node("Value").text = cardData.value
+	set_big_card_value(column, cardData.value)
 
+func _on_BigCard_clicked(column, value):
+	var newValue = find_next_card_in_column(column, value).value
+	set_big_card_value(column, newValue)
+
+func find_next_card_in_column(column, value):
+	var nextCardIndex = -1
+	for n in deck.cards.size():
+		var card = deck.cards[n]
+		if card.column == column:
+			# note the index of where this particular column starts in the deck
+			if nextCardIndex == -1:
+				nextCardIndex = n
+			if card.value == value:
+				# if at the end of the deck, return the first card in the column
+				if n + 1 == deck.cards.size():
+					break
+				var nextCard = deck.cards[n + 1]
+				# if the next card is part of this column, return it
+				if nextCard.column == column: 
+					nextCardIndex = n + 1
+	return deck.cards[nextCardIndex]
 func _on_Home_pressed():
 	get_tree().change_scene("res://Scenes/Home.tscn")
 
 func _on_Minimize_toggled(button_pressed):
 	if(button_pressed):
 		$UI/DrawerPanel/AnimationPlayer.play("minimize_drawer")
-		$UI/BigCards/AnimationPlayer.play("slide_cards_down")
+		$UI/BigCardsArea/AnimationPlayer.play("slide_cards_down")
 	else:
 		$UI/DrawerPanel/AnimationPlayer.play_backwards("minimize_drawer")
-		$UI/BigCards/AnimationPlayer.play_backwards("slide_cards_down")
+		$UI/BigCardsArea/AnimationPlayer.play_backwards("slide_cards_down")
+
+func set_big_card_value(column: int, value: String):
+	$UI/BigCardsArea/BigCards.get_child(column).get_node("Value").text = value
