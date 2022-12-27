cede20f2da54d572d813f0f7fdc3284d7ae955eb
Stephanie Cruz <stephanie@csma.technology>
2022-12-27 18:13:40 -0500

    feat(global-styles): added background, fonts, card styling (#23)
    * added backgrounds, fonts, and card colors
    
    * global styling
    
    * changed font on cards

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -4,21 +4,27 @@ class_name Card
 
 signal card_clicked(cardData)
 
+const whiteCard = preload("res://Styles/WhiteCard.tres")
+
 var data: CardData
 
 func _ready(): 
 	$Value.text = data.value
 	if data.color != "":
-		$Background.color = Color(data.color)
+		$Background.get_stylebox("Card").set("bg_color", data.color)
 
 func _init(data: CardData = CardData.new()):
 	self.data = data
 
-
-func _on_ColorRect_gui_input(event):
-	if event is InputEventScreenTouch and !event.pressed:
-		emit_signal("card_clicked", self.data)
-
 func set_color(new_color: Color):
 	data.color = new_color.to_html()
-	$Background.color = new_color
+	var newStyle = StyleBoxFlat.new()
+	newStyle.set_bg_color(data.color)
+	newStyle.set_corner_radius_all(8)
+	newStyle.set_border_width_all(1)
+	newStyle.set_border_color(Color.black)
+	$Background.set("custom_styles/panel", newStyle)
+
+func _on_Card_gui_input(event):
+	if event is InputEventScreenTouch and !event.pressed:
+		emit_signal("card_clicked", self.data)
