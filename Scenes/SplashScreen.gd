8a0a3324aa4416fed9c96ba8b6ea23a7fbdd83b4
Stephanie Cruz <exscruzme@gmail.com>
2022-12-29 00:53:19 -0500

    splash screen final for now

--- Scenes/SplashScreen.gd
+++ Scenes/SplashScreen.gd
@@ -9,6 +9,8 @@ func _ready():
 	tween.parallel().tween_property($Branding/DaydreamTitle, "scale", Vector2(0.1, 0.1), 0.5)
 	tween.tween_property($Branding/Label, "custom_colors/font_color", Color.white, 0.1)
 	tween.parallel().tween_property($Branding/DaydreamTitle, "position:x", 548.0, 0.5).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.tween_property($UI/Background, "modulate", Color(0, 0, 0, 0), 0.5)
+	tween.parallel().tween_property($UI/HomeText, "visible", true, 0)
 	
 func _on_Timer_timeout():
 	get_tree().change_scene("res://Scenes/Home/Home.tscn")
--- Scripts/Home.gd
+++ Scripts/Home.gd
@@ -3,9 +3,6 @@ extends Node2D
 const Deck = preload("res://Scenes/Home/Deck.tscn")
 
 func _ready():
-	var tween = get_tree().create_tween()
-	tween.tween_property($UI/ColorRect2, "modulate", Color(0, 0, 0, 0), 0.5)
-	tween.connect("finished", self, "_on_Tween_done")
 	GameAnalytics.init() # Sandbox mode for now
 	randomize()
 	
@@ -19,9 +16,6 @@ func _ready():
 			deck.deckName = deckMetadata.name
 			deck.deckId = deckMetadata.refId
 			$UI/ScrollContainer/Decks.add_child(deck)
-
-func _on_Tween_done():
-	$UI/ColorRect2.queue_free()
 
 func _on_NewDeckButton_pressed():
 	UserDataUtils.set_active_deck_id(-1)
