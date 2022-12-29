eed77dc1d405081c13d0afe023a4b28f0a5a22d6
Stephanie Cruz <exscruzme@gmail.com>
2022-12-29 02:24:19 -0500

    feat(home-styling): styled home screen according to mockups

--- Scripts/Deck.gd
+++ Scripts/Deck.gd
@@ -4,18 +4,3 @@ class_name Deck
 
 var deckName: String
 var deckId: int
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$TopCard/DeckName.text = deckName
-
-
-func _on_TopCard_gui_input(event:InputEvent):
-	if event is InputEventScreenTouch and !event.pressed:
-		UserDataUtils.set_active_deck_id(deckId)
-		get_tree().change_scene("res://Scenes/Play/PlayDeck.tscn")
-
-
-func _on_Edit_pressed():
-	UserDataUtils.set_active_deck_id(deckId)
-	get_tree().change_scene("res://Scenes/Edit/EditDeck.tscn")
--- Scripts/Home.gd
+++ Scripts/Home.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
-const Deck = preload("res://Scenes/Home/Deck.tscn")
+const UserDeck = preload("res://Scenes/Home/UserDeck.tscn")
 
 func _ready():
 	GameAnalytics.init() # Sandbox mode for now
 	randomize()
-	
+
 	UserDataUtils.initialize_app_data()
-	
+
 	var deckFiles = UserDataUtils.get_all_deck_files()
 	if not deckFiles.empty():
 		for deckFile in deckFiles:
 			var deckMetadata = UserDataUtils.get_deck_metadata(deckFile)
-			var deck = Deck.instance()
+			var deck = UserDeck.instance()
 			deck.deckName = deckMetadata.name
 			deck.deckId = deckMetadata.refId
 			$UI/ScrollContainer/Decks.add_child(deck)
@@ -20,3 +20,4 @@ func _ready():
 func _on_NewDeckButton_pressed():
 	UserDataUtils.set_active_deck_id(-1)
 	get_tree().change_scene("res://Scenes/Edit/EditDeck.tscn")
+	$UI/ScrollContainer/Decks.move_child($UI/ScrollContainer/Decks/NewDeck, $UI/ScrollContainer/Decks.get_child_count() - 1)
