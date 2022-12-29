fdcac428141d036ef98e0f6d259ada44a66453bb
Stephanie Cruz <exscruzme@gmail.com>
2022-12-29 14:47:40 -0500

    chore(cleanup-home): reorganized scripts, fixed new deck button, modulate edit on press

--- Scripts/UserDeck.gd
+++ Scripts/UserDeck.gd
@@ -14,3 +14,7 @@ func _on_TopCard_gui_input(event:InputEvent):
 func _on_EditButton_pressed():
 	UserDataUtils.set_active_deck_id(deckId)
 	get_tree().change_scene("res://Scenes/Edit/EditDeck.tscn")
+
+func _on_EditButton_button_down():
+	var tween = get_tree().create_tween()
+	tween.tween_property($EditButton, "modulate", Color("60B2FF"), 0)
